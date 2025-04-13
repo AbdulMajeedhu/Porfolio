71588096d6fdcd0b1ee36abb02aed9172381eefe
AbdulMajeedhu <abdulmajeedhu6283@gmail.com>
2025-04-13 12:47:56 +0530

    success

--- App.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', function() {
-        console.log('Menu toggled'); // Check if this appears in console
-        navLinks.classList.toggle('active');
-    });
-});
--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import './App.css';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './header';
-import About from './About';
-import Skill from './Skill';
-import Project from './Project';
-// import Resume from './Resume';
-import Contact from './Contact';
-import Footer from './Footer';
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
 
-
-function App() {
-  return (
-  <div>
-  <Header/>
-  <About/>
-  <Skill/>
-  <Project/>
-  {/* <Resume/> */}
-  <Contact/>
-  <Footer/>
-  </div>
-
-  );
-}
-
-export default App;
+    hamburger.addEventListener('click', function() {
+        console.log('Menu toggled'); // Check if this appears in console
+        navLinks.classList.toggle('active');
+    });
+});
